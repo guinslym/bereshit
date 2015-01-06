631d80536f5784c58df656234ee37ebcc639312f
Guinsly Mondesir <guinslym @gmail.com>
2015-01-06 03:24:28 -0500

    cleaning the spec

--- spec/Bereshit_spec.rb
+++ spec/Bereshit_spec.rb
@@ -15,9 +15,15 @@ describe Bereshit do
   end
 
   describe ".paragraphs" do
+    lines = Bereshit.paragraphs(3, false, false, 'english')
+
     it "should return 3 paragraphs" do
-      lines = Bereshit.paragraphs(3, false, false, 'english')
-      expect(lines.size).to be > 2
+      #it will not return an array of 3
+      #So I have to split it to see if there is 3 paragraphs
+      expect(lines.split('<p>').size).to be >= 4
+    end
+    it 'includes a paragraph tag' do
+      expect(lines.include?('<p>')).to be true
     end
   end
 
